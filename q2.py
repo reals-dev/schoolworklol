0741e7ed48578560c8b4a4f91073df8a5f5fd2fe
reals-dev <realsbvdevelopment@gmail.com>
2023-10-09 11:18:54 +0100

    hello

---
+++ q2.py
@@ -0,0 +1,3 @@
+check = int(input('i have 10 chocolate bars, how much would you like to eat? '))
+left = 10 - check
+print('oh that leaves me with ', left, 'left')
---
+++ q3.py
@@ -0,0 +1,3 @@
+km = int(input('what is the distance in km? '))
+miles = km * 0.62137
+print(km, 'km is ', miles, 'miles')
